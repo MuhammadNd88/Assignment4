51dcd8c7fbc610a23463a6d5767e44f82dd132fe
Muhammad Nadeem <muhammad.nadeem@edu.sait.ca>
2019-02-05 18:13:02 -0700

    changed

---
+++ JavaScript/exercises-day1.js
@@ -0,0 +1,26 @@
+
+
+function sumFiveNumber(x,y,z,p,q){
+
+
+return x+y+z+p+q;
+
+    if(a <= 6 && b<=6 )
+{
+    if(c <= 6 && d<=6 ){
+        if(e<=6){
+            return a+b+c+d+e;
+
+        }
+        console.log('wrong Numbers');
+    }
+    console.log('wrong Numbers');
+}
+else {
+    console.log('wrong Numbers');
+
+}
+
+
+}
+console.log(sumFiveNumber(2, 2, 3, 4, 5));
